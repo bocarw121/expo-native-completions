c47bdca317ea28a303a693d07f2df93c5eae6d35
bocarw121 <bocaralhassanwane@gmail.com>
2024-06-26 08:53:39 -0400

    Remove isExpoProject check and update tests
    Replace the isExpoProject with event listeners in package.json
    
    Also updated and cleaned up tests

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,14 @@
 import * as vscode from "vscode";
-
-import { isExpoProject } from "./utils";
 import { kotlinHoverProvider, kotlinProviders } from "./completions/kotlin";
 import { swiftHoverProvider, swiftProviders } from "./completions/swift";
 
 export function activate(context: vscode.ExtensionContext) {
-  if (isExpoProject()) {
-    context.subscriptions.push(...kotlinProviders);
-    context.subscriptions.push(...swiftProviders);
-    context.subscriptions.push(kotlinHoverProvider);
-    context.subscriptions.push(swiftHoverProvider);
-  }
+  context.subscriptions.push(...kotlinProviders);
+  context.subscriptions.push(...swiftProviders);
+  context.subscriptions.push(kotlinHoverProvider);
+  context.subscriptions.push(swiftHoverProvider);
+
+  vscode.window.showInformationMessage("Expo Native Completions enabled!");
 }
 
 export function deactivate() {}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
-export * from "./expo-check";
 export * from "./registerCompletionProvider";
 export * from "./registerHoverProvider";
